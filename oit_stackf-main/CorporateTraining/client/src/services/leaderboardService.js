ea47572664e9d37dc2d686dbe9d347981ca9dd60
Pratap Sanap <144205384+PRATAPSANAP1@users.noreply.github.com>
2026-06-12 14:41:18 +0530

    Delete oit_stackf-main directory

--- oit_stackf-main/CorporateTraining/client/src/services/leaderboardService.js
+++
@@ -1,8 +0,0 @@
-import api from './api';
-
-const leaderboardService = {
-  getLeaderboard: (params) => api.get('/leaderboard', { params }),
-};
-
-export default leaderboardService;
-
--- oit_stackf-main/backend/routes/voterRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getVoters } = require('../controllers/voterController');
-
-router.get('/voters', getVoters);
-
-module.exports = router;
