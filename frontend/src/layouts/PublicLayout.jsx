b1ef0b56b31749c1f3abd71ba1307c1290eb59ef
gayatripachpande99 <Gayatripachpande99@gmail.com>
2026-07-01 15:12:22 +0530

    feat: Hide navbar on login and register pages

--- frontend/src/layouts/PublicLayout.jsx
+++ frontend/src/layouts/PublicLayout.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Navbar from '../components/Navbar/Navbar';
 import Chatbot from '../components/Chatbot/Chatbot';
 
 const PublicLayout = () => {
+  const location = useLocation();
+  const path = location.pathname.toLowerCase().replace(/\/$/, '');
+  const isAuthPage = path === '/login' || path === '/register';
+
   return (
     <>
-      <Navbar />
+      {!isAuthPage && <Navbar />}
       <Outlet />
       <Chatbot />
     </>
