73720396f749124bb9c1239fe40ea59eba830095
gayatripachpande99 <Gayatripachpande99@gmail.com>
2026-08-07 18:49:57 +0530

    fix(kyc): import Link from react-router-dom in PartnerKyc.jsx to fix ReferenceError

--- frontend/src/modules/partner/kyc/PartnerKyc.jsx
+++ frontend/src/modules/partner/kyc/PartnerKyc.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { useAuthStore } from '../../../app/store/authStore';
 import { usePartnerStore } from '../../../app/store/partnerStore';
 import { useTheme, makeS } from '../../../contexts/ThemeContext';
