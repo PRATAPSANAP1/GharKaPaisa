16661e034cccc2f4a1aefca30c2e7a5b77918a2d
gayatripachpande99 <Gayatripachpande99@gmail.com>
2026-08-31 17:43:09 +0530

    fix: add missing useEffect import in KYCSubmission.jsx

--- frontend/src/modules/employee/profile/KYCSubmission.jsx
+++ frontend/src/modules/employee/profile/KYCSubmission.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useTheme } from '../../../contexts/ThemeContext';
 import { FaArrowLeft, FaIdCard, FaCheckCircle, FaFileAlt, FaUpload } from 'react-icons/fa';
